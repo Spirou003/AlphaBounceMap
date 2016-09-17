9c89cd82ee47150d3bb5a466e938ddc5a4dc6cfe
PoulainJulien <poulain-julien@hotmail.com>
2016-09-17 21:29:52 +0200

    Add help, remove save when KeyboardInterrupt

--- explore.py
+++ explore.py
@@ -12,68 +12,76 @@ if (len(sys.argv) < 2):
 else:
     playername = sys.argv[1]
 Str = playername+".txt"
+
+filename = Str
+list = core.load(filename)
+Str = ""
+print 'Tapez "help" pour obtenir de l\'aide'
+b = False
 try:
-    filename = Str
-    list = core.load(filename)
-    Str = ""
-    print 'Pour desexplorer, tapez "d" devant les coordonnees "x y"'
-    try:
-        b = False
-        while (not core.oneIn(exitwords, Str)):
-            Str = raw_input("> ")
-            if (core.oneIn(mapwords, Str)):
+    while (not core.oneIn(exitwords, Str)):
+        Str = raw_input("> ")
+        Strlist = Str.strip().lower().split(" ")
+        try:
+            if (core.oneIn(Strlist, "help")):
+                if (len(Strlist) == 1):
+                    print 'Tapez "x y" pour marquer le secteur aux coordonnees (x, y) exploree.'
+                    print 'Tapez "zone x1 y1 x2 y2" pour marquer comme explore chaque secteur situe dans le rectangle decrit par les coordonnees (x1, y1) et (x2, y2).'
+                    print 'Dans les commandes precedentes, l\'option "d" a pour effet de marquer comme non explore.'
+                    print "Pour quitter ce script, entrez l'un des mots suivants: "+str(exitwords)
+                    print 'Pour les autres commandes, tapez "help commandname" pour obtenir des informations supplementaires.'
+                    print 'Valeurs possibles de "commandname" pour generer la carte: '+str(mapwords)
+                    print 'Valeurs possibles de "commandname" pour sauvegarder les donnees: '+str(savewords)
+                else:
+                    pass
+            elif (core.oneIn(mapwords, Str)):
                 core.makeMap(playername, list)
             elif (core.oneIn(savewords, Str)):
                 core.save(filename, list, None)
             elif (core.oneIn(exitwords, Str)):
-                pass
+                pass #nothing to do
             else:
-                try:
-                    args = Str.strip().split(" ")
-                    if (core.oneIn(args, "zone")):
-                        args.remove("zone")
-                        b = ('d' in args)
+                if (core.oneIn(Strlist, "zone")):
+                    Strlist.remove("zone")
+                    b = ('d' in Strlist)
+                    if (b):
+                        Strlist.remove("d")
+                    (x1, y1, x2, y2) = Strlist
+                    x1 = int(x1)
+                    y1 = int(y1)
+                    x2 = int(x2)
+                    y2 = int(y2)
+                    (x1, x2) = (min(x1, x2), max(x1, x2))
+                    (y1, y2) = (min(y1, y2), max(y1, y2))
+                    for x in xrange(x1, x2+1):
+                        for y in xrange(y1, y2+1):
+                            try:
+                                if (b):
+                                    core.remove(list, x, y)
+                                else:
+                                    core.add(list, x, y)
+                            except ValueError, e:
+                                print e
+                            except Exception, e:
+                                traceback.print_exc()
+                else:
+                    b = ('d' in Strlist)
+                    if (b):
+                        Strlist.remove("d")
+                    (x, y) = Strlist
+                    x = int(x)
+                    y = int(y)
+                    try:
                         if (b):
-                            args.remove("d")
-                        (x1, y1, x2, y2) = args
-                        x1 = int(x1)
-                        y1 = int(y1)
-                        x2 = int(x2)
-                        y2 = int(y2)
-                        (x1, x2) = (min(x1, x2), max(x1, x2))
-                        (y1, y2) = (min(y1, y2), max(y1, y2))
-                        for x in xrange(x1, x2+1):
-                            for y in xrange(y1, y2+1):
-                                try:
-                                    if (b):
-                                        core.remove(list, x, y)
-                                    else:
-                                        core.add(list, x, y)
-                                except ValueError, e:
-                                    print e
-                                except Exception, e:
-                                    print "une erreur est survenue: "+str(e)
-                    else:
-                        b = ('d' in args)
-                        if (b):
-                            args.remove("d")
-                        (x, y) = args
-                        x = int(x)
-                        y = int(y)
-                        try:
-                            if (b):
-                                core.remove(list, x, y)
-                            else:
-                                core.add(list, x, y)
-                        except ValueError, e:
-                            print e
-                        except Exception, e:
-                            print "une erreur est survenue: "+str(e)
-                except Exception, e:
-                    traceback.print_exc()
-    except KeyboardInterrupt, e:
-        core.save(filename, list, None)
-    except Exception, e:
-        print e
+                            core.remove(list, x, y)
+                        else:
+                            core.add(list, x, y)
+                    except ValueError, e:
+                        print e
+        except Exception, e:
+            traceback.print_exc()
+except KeyboardInterrupt, e:
+    pass
 except Exception, e:
-    print e
+    traceback.print_exc()
+#
