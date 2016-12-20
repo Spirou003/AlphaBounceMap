5b4fe7bea586f72ee836b74acb2972a38133bcb0
PoulainJulien <poulain-julien@hotmail.com>
2016-12-20 21:42:44 +0100

    Merge words files

--- Main.py
+++ Main.py
@@ -21,32 +21,27 @@ def Main():
     planet_names = list(planets.keys())
     reservedwords = ["zone", "d", "help"] + planet_names
 
-    mapwords = Core.loadWords(CONFIGDIR+"words_map.txt")
-    Core.removefromlist(mapwords, reservedwords)
-    if (len(mapwords) == 0):
-        mapwords.append("map")
-
-    savewords = Core.loadWords(CONFIGDIR+"words_save.txt")
-    Core.removefromlist(savewords, reservedwords)
-    if (len(savewords) == 0):
-        savewords.append("save")
-
-    viewwords = Core.loadWords(CONFIGDIR+"words_view.txt")
-    Core.removefromlist(viewwords, reservedwords)
-    if (len(viewwords) == 0):
-        viewwords.append("view")
-
-    targetwords = Core.loadWords(CONFIGDIR+"words_target.txt")
-    Core.removefromlist(targetwords, reservedwords)
-    if (len(targetwords) == 0):
-        targetwords.append("target")
-
-    exitwords = Core.loadWords(CONFIGDIR+"words_exit.txt")
-    Core.removefromlist(exitwords, reservedwords)
+    commands = Core.readconfigfile(CONFIGDIR+"words.txt")
+    
+    #make sure that dictionnary contains all used keys
+    for key in ["map", "save", "target", "view", "exit"]:
+        if (key not in commands or "words" not in commands[key]):
+            commands[key] = {"words":key}
+    #
+    #get set of words for each command (instead of space-separated string)
+    #keep only non reserved words, but at least one
+    for key in commands:
+        if "words" in commands[key]:
+            commands[key]["words"] = set(commands[key]["words"].split())
+            Core.removefromlist(commands[key]["words"], reservedwords)
+            if (len(commands[key]["words"]) == 0):
+                commands[key]["words"].add(key)
+    #
+    #use some standard words to exit prompt
     exitwords_reserved = ("quit", "exit")
     for exit_word in exitwords_reserved:
-        if (not exit_word in exitwords):
-            exitwords.append(exit_word)
+        if (not exit_word in commands["exit"]["words"]):
+            commands["exit"]["words"].add(exit_word)
     #
     playername = ""
     if (len(sys.argv) >= 2):
@@ -64,7 +59,7 @@ def Main():
     Strlist = []
     print('Tapez "help" pour obtenir de l\'aide')
     try:
-        while (not Core.oneIn(exitwords, Strlist)):
+        while (not Core.oneIn(commands["exit"]["words"], Strlist)):
             Str = raw_input("> ").strip().lower()
             if (Str == ""):
                 continue
@@ -77,14 +72,14 @@ def Main():
                         print("Entrez le nom d'une planete pour marquer chacun de ses secteurs comme explore. Exception pour la Terre.")
                         print('Liste des planetes: '+str(planet_names))
                         print('Dans les commandes precedentes, l\'option "d" a pour effet de marquer comme non explore.')
-                        print("Pour quitter ce script, entrez l'un des mots suivants: "+str(exitwords))
+                        print("Pour quitter ce script, entrez l'un des mots suivants: "+str(commands["exit"]["words"]))
                         print('Pour les autres commandes, tapez "help commandname" pour obtenir des informations supplementaires.')
-                        print('Valeurs possibles de "commandname" pour generer la carte: '+str(mapwords))
-                        print('Valeurs possibles de "commandname" pour sauvegarder les donnees: '+str(savewords))
+                        print('Valeurs possibles de "commandname" pour generer la carte: '+str(commands["map"]["words"]))
+                        print('Valeurs possibles de "commandname" pour sauvegarder les donnees: '+str(commands["save"]["words"]))
                     else:
-                        if (Core.oneIn(mapwords, Strlist)):
+                        if (Core.oneIn(commands["map"]["words"], Strlist)):
                             print("Coming soon")
-                        elif (Core.oneIn(savewords, Strlist)):
+                        elif (Core.oneIn(commands["save"]["words"], Strlist)):
                             print("Coming soon")
                         elif (Core.oneIn(planet_names, Strlist)):
                             print("Coming soon")
@@ -92,20 +87,20 @@ def Main():
                             Strlist.remove("help")
                             tempstring = " ".join(Strlist)
                             print('Aucune commande du nom de "'+tempstring+'"')
-                elif (Core.oneIn(mapwords, Strlist)):
+                elif (Core.oneIn(commands["map"]["words"], Strlist)):
                     Map.makeMap(playername, playerdata, planets)
-                elif (Core.oneIn(savewords, Strlist)):
+                elif (Core.oneIn(commands["save"]["words"], Strlist)):
                     Data.save(playername, playerdata)
-                elif (Core.oneIn(viewwords, Strlist)):
+                elif (Core.oneIn(commands["view"]["words"], Strlist)):
                     os.system(Map.getMapFilename(playername))
-                elif (Core.oneIn(targetwords, Strlist)):
+                elif (Core.oneIn(commands["target"]["words"], Strlist)):
                     print('Entrez vos nouveaux objectifs:')
-                    while (not Core.oneIn(exitwords, Strlist)):
+                    while (not Core.oneIn(commands["exit"]["words"], Strlist)):
                         Str = raw_input("==> ").strip().lower()
                         if (Str == ""):
                             continue
                         Strlist = Str.split(" ")
-                        if (Core.oneIn(exitwords, Strlist)):
+                        if (Core.oneIn(commands["exit"]["words"], Strlist)):
                             pass #nothing to do
                         else:
                             (coords, explore) = parsecoords(Str, Strlist, planet_names, planets)
@@ -114,7 +109,7 @@ def Main():
                             else:
                                 Data.deltarget(playerdata, coords)
                     Strlist = [] #don't exit immediately after that
-                elif (Core.oneIn(exitwords, Strlist)):
+                elif (Core.oneIn(commands["exit"]["words"], Strlist)):
                     pass #nothing to do
                 else:
                     (coords, explore) = parsecoords(Str, Strlist, planet_names, planets)
